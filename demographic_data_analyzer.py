40030673772c4523976923009ff64b11318acf94
lucastort1 <159059797+lucastort1@users.noreply.github.com>
2024-10-02 20:20:43 -0300

    codigo-novo

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -1,47 +1,43 @@
-import pandas as pd
+import pandas as pd # Importa a biblioteca Pandas para manipulação de dados
 
 
 def calculate_demographic_data(print_data=True):
-    # Read data from file
+    # Lê os dados de um arquivo
     df = None
 
-    # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
+    # Quantas pessoas de cada raça estão representadas no dataset
     race_count = None
 
-    # What is the average age of men?
+    # Qual é a idade média dos homens
     average_age_men = None
 
-    # What is the percentage of people who have a Bachelor's degree?
+     # Qual é a porcentagem de pessoas que têm um diploma de bacharel
     percentage_bachelors = None
 
-    # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
-    # What percentage of people without advanced education make more than 50K?
-
-    # with and without `Bachelors`, `Masters`, or `Doctorate`
+    # Pessoas com e sem educação avançada
     higher_education = None
     lower_education = None
 
-    # percentage with salary >50K
+     # Porcentagem de pessoas com salário maior de 50 mil
     higher_education_rich = None
     lower_education_rich = None
 
-    # What is the minimum number of hours a person works per week (hours-per-week feature)?
+     # Qual é o número mínimo de horas que uma pessoa trabalha por semana
     min_work_hours = None
 
-    # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
+    # Porcentagem de pessoas que trabalham o número mínimo de horas por semana e têm um salário maior que 50 mil
     num_min_workers = None
 
     rich_percentage = None
 
-    # What country has the highest percentage of people that earn >50K?
+    # Qual país tem a maior porcentagem de pessoas que ganham mais de 50 mil
     highest_earning_country = None
     highest_earning_country_percentage = None
 
-    # Identify the most popular occupation for those who earn >50K in India.
+    # Qual a ocupação mais popular para aqueles que ganham mais de 50 mil na Índia
     top_IN_occupation = None
 
-    # DO NOT MODIFY BELOW THIS LINE
-
+    # Se `print_data` for True, imprime os resultados calculados acima
     if print_data:
         print("Number of each race:\n", race_count) 
         print("Average age of men:", average_age_men)
@@ -54,6 +50,7 @@ def calculate_demographic_data(print_data=True):
         print(f"Highest percentage of rich people in country: {highest_earning_country_percentage}%")
         print("Top occupations in India:", top_IN_occupation)
 
+    # Retorna um dicionário contendo todas as métricas calculadas
     return {
         'race_count': race_count,
         'average_age_men': average_age_men,
